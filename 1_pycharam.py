2060d1a605f3c345a6b0b41958fd9dd413835f85
jawadbbas <168073996+jawadbbas@users.noreply.github.com>
2025-02-10 11:02:45 +0500

    Add files via upload

---
+++ 1_pycharam.py
@@ -0,0 +1,10 @@
+print("Hello to the world")
+while True:
+    name = input('>')
+    print('Wc to the show of git and github first file for  repository :',name)
+    if name =='abbas':
+        print(f'wow amazing name abbas,{name} :')
+        print('as owner enters the room the loop will terminate : ')
+        break
+    else:
+        print('Wc to the show of abbas',name)
